ff51375c61551b4a717b392b6e1fdebc32dfe184
t0mri <thamaraiselvanr27@gmail.com>
2023-11-09 11:38:08 +0530

    implementation: removed qr code from 'download certificate' page

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const hamburger = document.querySelector("#hamburger");
 const mobileNav = document.querySelector("#mobile-nav");
 const pageAnchors = document.querySelectorAll(".anchor");
 const navItems = [
-    ...document.querySelectorAll("nav a"),
-    ...document.querySelectorAll("#mobile-nav a h3"),
+  ...document.querySelectorAll("nav a"),
+  ...document.querySelectorAll("#mobile-nav a h3"),
 ];
 const sun = document.querySelector(".fa-sun");
 const moon = document.querySelector(".fa-moon");
@@ -21,140 +21,121 @@ const cards = document.querySelectorAll(".card");
 
 // color scheme
 const switchColorSchemeIcons = (isDark) =>
-    isDark
-        ? ((sun.style.display = "none"), (moon.style.display = "block"))
-        : ((sun.style.display = "block"), (moon.style.display = "none"));
+  isDark
+    ? ((sun.style.display = "none"), (moon.style.display = "block"))
+    : ((sun.style.display = "block"), (moon.style.display = "none"));
 
 const switchColorScheme = (key) => {
-    key
-        ? (html.dataset.colorScheme = "dark")
-        : (html.dataset.colorScheme = "light");
+  key
+    ? (html.dataset.colorScheme = "dark")
+    : (html.dataset.colorScheme = "light");
 
-    switchColorSchemeIcons(key);
+  switchColorSchemeIcons(key);
 };
 
 switchColorScheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
 
 // hamburger functionalities
 hamburger.onclick = () => {
-    mobileNav.style.width = `100%`;
+  mobileNav.style.width = `100%`;
 };
 [...mobileNav.children].forEach(
-    (child) => (child.onclick = () => (mobileNav.style.width = `0%`))
+  (child) => (child.onclick = () => (mobileNav.style.width = `0%`)),
 );
 
 document.querySelectorAll("section").forEach(
-    (section) =>
-        (section.onclick = () => {
-            if (mobileNav.style.width == `100%`) {
-                mobileNav.style.width = `0%`;
-            }
-        })
+  (section) =>
+    (section.onclick = () => {
+      if (mobileNav.style.width == `100%`) {
+        mobileNav.style.width = `0%`;
+      }
+    }),
 );
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            navItems.forEach((item) => {
-                if (item.innerHTML == entry.target.dataset.page) {
-                    item.classList.add("active");
-                } else {
-                    item.classList.remove("active");
-                }
-            });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      navItems.forEach((item) => {
+        if (item.innerHTML == entry.target.dataset.page) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
         }
-    });
+      });
+    }
+  });
 });
 
 pageAnchors.forEach((anchor) => observer.observe(anchor));
 
 for (let i = 0; i < 10; i++) {
-    cards.forEach((card) => marquee.appendChild(card.cloneNode(true)));
+  cards.forEach((card) => marquee.appendChild(card.cloneNode(true)));
 }
 let not = true;
 document.querySelectorAll(".card").forEach((card) => {
-    card.onmouseover = () => marquee.stop();
-    card.onmouseleave = () => marquee.start();
+  card.onmouseover = () => marquee.stop();
+  card.onmouseleave = () => marquee.start();
 
-    card.ontouchstart = () => {
-        marquee[!not ? "start" : "stop"]();
-        not = !not;
-    };
+  card.ontouchstart = () => {
+    marquee[!not ? "start" : "stop"]();
+    not = !not;
+  };
 });
 
 function numberFilter(e) {
-    if (
-        !/[0-9]/.test(e.key) &&
-        e.key != "Backspace" &&
-        e.key != "ArrowLeft" &&
-        e.key != "ArrowRight"
-    )
-        e.preventDefault();
+  if (
+    !/[0-9]/.test(e.key) &&
+    e.key != "Backspace" &&
+    e.key != "ArrowLeft" &&
+    e.key != "ArrowRight"
+  )
+    e.preventDefault();
 }
 
 // firebase
 const db = firebase.firestore();
 
 function sendMessage(e, name, phone, email, message) {
-    e.preventDefault();
+  e.preventDefault();
 
-    db.collection("messages")
-        .add({
-            name,
-            phone,
-            email,
-            message,
-            date: new Date().toISOString(),
-        })
-        .then((r) => {
-            console.log(r);
-            e.target.reset();
-            alert("Response received!");
-        })
-        .catch((e) => console.error(e));
+  db.collection("messages")
+    .add({
+      name,
+      phone,
+      email,
+      message,
+      date: new Date().toISOString(),
+    })
+    .then((r) => {
+      console.log(r);
+      e.target.reset();
+      alert("Response received!");
+    })
+    .catch((e) => console.error(e));
 
-    emailjs.send("contactForm", "ContactFormID", {
-        message: `Name: ${name}\n
+  emailjs.send("contactForm", "ContactFormID", {
+    message: `Name: ${name}\n
     Phone: ${phone}\n
     E-mail: ${email}\n
     Message: ${message}`,
-        user_email: email,
-    });
+    user_email: email,
+  });
 }
 
 const getCertificate = (e, id) => {
-    e.preventDefault();
-    firebase
-        .storage()
-        .ref(`certificates/${id}.jpg`)
-        .getDownloadURL()
-        .then(async (res) => {
-            errMsg.style.display = "none";
-            certificateImg.src = res;
-            certificateDownload.href = res;
-            await fetch("https://t.ly/api/v1/link/shorten", {
-                method: "POST",
-                headers: {
-                    Authorization:
-                        "Bearer 3Ap5EPTk2Yw5llJ80BWxVC6wfwNOz6IVauenRPxLH4l6P47Tfc3poTSR0Pxb",
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: JSON.stringify({
-                    long_url: res,
-                    domain: "https://t.ly/",
-                }),
-            }).then((response) =>
-                response.json().then((shrtndlnk) => {
-                    certificateQrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${shrtndlnk.short_url}`;
-                })
-            );
-            certificateDownload.style.display = "block";
-            certificateQr.style.display = "block";
-        })
-        .catch((err) => {
-            errMsg.innerText = "Certificate cannot be found";
-            errMsg.style.display = "block";
-            certificateQr.style.display = "none";
-        });
+  e.preventDefault();
+  firebase
+    .storage()
+    .ref(`certificates/${id}.jpg`)
+    .getDownloadURL()
+    .then(async (res) => {
+      errMsg.style.display = "none";
+      certificateImg.src = res;
+      certificateDownload.href = res;
+      certificateDownload.style.display = "block";
+    })
+    .catch((err) => {
+      errMsg.innerText = "Certificate cannot be found";
+      errMsg.style.display = "block";
+    });
 };
